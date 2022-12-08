d00b7f5a011220ba9be3193fc17884dde3942dc5
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-12-08 17:02:25 -0500

    Add solution for day 8, part 1

--- day8/code1.c
+++ day8/code1.c
@@ -5,15 +5,20 @@
 #include <stdlib.h>
 
 
+// Definitions
 #define MAX_LINE_LEN UCHAR_MAX
 
 
+// Type definition of a tree
 typedef struct GridTree {
     int grid_value;
     char height;
 } tree_t;
 
 
+/// @brief Get the number of rows in the forest
+/// @param fp A pointer to the open file
+/// @return The number of rows
 unsigned int get_num_rows(FILE *fp) {
 
     // Get a buffer to store characters
@@ -41,6 +46,10 @@ unsigned int get_num_rows(FILE *fp) {
 
 }
 
+
+/// @brief Get the number of columns in the forest
+/// @param fp A pointer to the open file
+/// @return The number of columns
 unsigned int get_num_cols(FILE *fp) {
 
     // Get a buffer to store characters
@@ -61,63 +70,132 @@ unsigned int get_num_cols(FILE *fp) {
 
 }
 
-/*
-void mark_from_left(char **tree_map, bool **visible_map, unsigned int num_rows, unsigned int num_cols) {
-    for (int row_index = 1; row_index < num_rows - 1; row_index++) {
-        // Get the tree line
-        char tree_line[MAX_LINE_LEN];
-        fgets(tree_line, sizeof(tree_line), tree_map);
-        if (feof(tree_map)) { fseek(tree_map, 0, SEEK_SET); return; }
+
+/// @brief Allocate memory for a char 2D matrix
+/// @param input_matrix A pointer to a char** buffer
+/// @param num_rows The number of rows in the matrix
+/// @param num_cols The number of columns in the matrix
+void allocate_char_matrix(char ***input_matrix, unsigned int num_rows, unsigned int num_cols) {
+    (*input_matrix) = malloc(num_rows * sizeof(char *));
+    for (int index = 0; index < num_rows; index++) {
+        (*input_matrix)[index] = malloc(num_cols * sizeof(char));
+    }
+}
 
 
-        tree_t leftmost_tree = { 0, tree_line[0] };
-        for (int col_index = 1; col_index < num_cols - 2; col_index++) {
+/// @brief Allocate memory for a bool 2D matrix
+/// @param input_matrix A pointer to a bool** buffer
+/// @param num_rows The number of rows in the matrix
+/// @param num_cols The number of columns in the matrix
+void allocate_bool_matrix(bool ***input_matrix, unsigned int num_rows, unsigned int num_cols) {
+    (*input_matrix) = malloc(num_rows * sizeof(int *));
+    for (int index = 0; index < num_rows; index++) {
+        (*input_matrix)[index] = malloc(num_cols * sizeof(int));
+    }
+}
+
+
+/// @brief Mark trees in the given buffer from the left side
+/// @param tree_map A pointer to a char** 2D matrix
+/// @param visible_map A pointer to a bool** 2D matrix to store the result
+/// @param num_rows The number of rows in the matrix
+/// @param num_cols The number of columns in the matrix
+void mark_from_left(char ***tree_map, bool ***visible_map, unsigned int num_rows, unsigned int num_cols) {
+    tree_t leftmost_tree;
+    for (int row_index = 0; row_index < num_rows; row_index++) {
+        leftmost_tree.grid_value = 0;
+        leftmost_tree.height = '0';
+        for (int col_index = 0; col_index < num_cols; col_index++) {
+
             // Parsing as string is fine
-            if (tree_line[col_index] > leftmost_tree.height) {
+            if (col_index == 0 || (*tree_map)[row_index][col_index] > leftmost_tree.height) {
                 leftmost_tree.grid_value = col_index;
-                leftmost_tree.height = tree_line[col_index];
-                visible_map[row_index][leftmost_tree.grid_value] = true;
-                if (tree_line[col_index] == '9') { break; }
+                leftmost_tree.height = (*tree_map)[row_index][col_index];
+                (*visible_map)[row_index][leftmost_tree.grid_value] = true;
+                if ((*tree_map)[row_index][col_index] == '9') { break; }
             }
+
         }
 
     }
-}*/
+}
 
 
-void init_visible_map(bool **visible_map, unsigned int num_rows, unsigned int num_cols) {
+/// @brief Rotate the given char** 2D matrix clockwise
+/// @param array_map A pointer to a char** 2D matrix
+/// @param num_rows The number of rows in the matrix
+/// @param num_cols The number of columns in the matrix
+void rotate_char_clockwise(char ***array_map, unsigned int num_rows, unsigned int num_cols) {
+    char **array_copy;
+    allocate_char_matrix(&array_copy, num_rows, num_cols);
+
     for (int row_index = 0; row_index < num_rows; row_index++) {
-        printf("a\n");
         for (int col_index = 0; col_index < num_cols; col_index++) {
-            printf("%i, %i\n", row_index, col_index);
-            if (row_index == 0 || row_index == num_rows - 1|| col_index == 0 || col_index == num_cols - 1) {
-                printf("T\n");
-                visible_map[row_index][col_index] = true;
-            }
-            else {
-                printf("F\n");
-                visible_map[row_index][col_index] = false;
-            }
+            array_copy[row_index][col_index] = (*array_map)[(num_cols-1) - col_index][row_index];
+        }
+    }
+    free(*array_map);
+    *array_map = array_copy;
+}
+
+
+/// @brief Rotate the given bool** 2D matrix clockwise
+/// @param array_map A pointer to a bool** 2D matrix
+/// @param num_rows The number of rows in the matrix
+/// @param num_cols The number of columns in the matrix
+void rotate_bool_clockwise(bool ***array_map, unsigned int num_rows, unsigned int num_cols) {
+    bool **array_copy;
+    allocate_bool_matrix((bool ***)&array_copy, num_rows, num_cols);
+
+    for (int row_index = 0; row_index < num_rows; row_index++) {
+        for (int col_index = 0; col_index < num_cols; col_index++) {
+            array_copy[row_index][col_index] = (*array_map)[(num_cols-1) - col_index][row_index];
+        }
+    }
+    free(*array_map);
+    *array_map = array_copy;
+
+}
+
+
+/// @brief Initialize the tree visibility matrix
+/// @param visible_map A pointer to the bool** 2D matrix
+/// @param num_rows The number of rows in matrix
+/// @param num_cols The number of columns in the matrix
+void init_visible_map(bool ***visible_map, unsigned int num_rows, unsigned int num_cols) {
+    for (int row_index = 0; row_index < num_rows; row_index++) {
+        for (int col_index = 0; col_index < num_cols; col_index++) {
+            (*visible_map)[row_index][col_index] = false;
         }
     }
 }
 
 
-void init_tree_map(FILE *fp, char **tree_map, unsigned int num_rows, unsigned int num_cols) {
+/// @brief Initialize the tree height matrix
+/// @param fp A pointer to the open input file
+/// @param tree_map A pointer to the char** 2D matrix
+/// @param num_rows The number of rows in matrix
+/// @param num_cols The number of columns in the matrix
+void init_tree_map(FILE *fp, char ***tree_map, unsigned int num_rows, unsigned int num_cols) {
     for (int row_index = 0; row_index < num_rows; row_index++) {
         char *row_buffer = malloc(num_cols+2);
         fgets(row_buffer, num_cols + 2, fp);
         for (int col_index = 0; col_index < num_cols; col_index++) {
-            tree_map[row_index][col_index] = row_buffer[col_index];
-            if (row_buffer[col_index] == '\n') { printf("Uh oh @ %i, %i\n", row_index, col_index); exit(1); } 
-            printf("%c", row_buffer[col_index]);
+            (*tree_map)[row_index][col_index] = row_buffer[col_index];
+            if (row_buffer[col_index] == '\n') {
+                printf("Error!");
+                exit(1);
+            } 
         }
-        printf("\n");
         free(row_buffer);
     }
 }
 
 
+/// @brief Display a tree map
+/// @param visible_map The tree map to display
+/// @param num_rows The number of rows in the matrix
+/// @param num_cols The number of columns in the matrix
 void display_tree_map(bool **visible_map, unsigned int num_rows, unsigned int num_cols) {
     for (int row_index = 0; row_index < num_rows; row_index++) {
         for (int col_index = 0; col_index < num_cols; col_index++) {
@@ -128,6 +206,11 @@ void display_tree_map(bool **visible_map, unsigned int num_rows, unsigned int nu
 }
 
 
+/// @brief Count the number of trees visible from the map
+/// @param treemap The visibility map
+/// @param num_rows The number of rows in the matrix
+/// @param num_cols The number of columns in the matrix
+/// @return The total number of trees visible
 unsigned int count_visible_trees(bool **treemap, unsigned int num_rows, unsigned int num_cols) {
     unsigned int total = 0;
     for (int row_index = 0; row_index < num_rows; row_index++) {
@@ -147,28 +230,25 @@ int main(int argc, char **argv) {
     unsigned int num_rows = get_num_rows(fp);
     unsigned int num_cols = get_num_cols(fp);
 
-    printf("# rows: %u\n", num_rows);
-    printf("# cols: %u\n", num_cols);
+    char **tree_map;
+    allocate_char_matrix(&tree_map, num_rows, num_cols);
+    init_tree_map(fp, &tree_map, num_rows, num_cols);
 
-    char **tree_map = malloc(num_rows * num_cols * sizeof(char));
-    for (int index = 0; index < num_rows; index++) {
-        tree_map[index] = malloc(num_cols * sizeof(int));
+    bool **visible_map;
+    allocate_bool_matrix(&visible_map, num_rows, num_cols);
+    init_visible_map(&visible_map, num_rows, num_cols);
+
+    for (int index = 0; index < 4; index++) {
+        mark_from_left(&tree_map, &visible_map, num_rows, num_cols);
+
+        rotate_char_clockwise(&tree_map, num_rows, num_cols);
+        rotate_bool_clockwise(&visible_map, num_rows, num_cols);
     }
-    init_tree_map(fp, tree_map, num_rows, num_cols);
-
-
-    bool **visible_map = malloc(num_rows * num_cols * sizeof(int));  // TODO: FIx later?
-    for (int index = 0; index < num_rows; index++) {
-        visible_map[index] = malloc(num_cols * sizeof(int));
-    }
-
-    init_visible_map(visible_map, num_rows, num_cols);
-
-    //mark_from_left(fp, visible_map, num_rows, num_cols);
-
-    display_tree_map(visible_map, num_rows, num_cols);
 
     unsigned int total = count_visible_trees(visible_map, num_rows, num_cols);
     printf("Total trees visible: %u\n", total);
+
+    free(tree_map);
+    free(visible_map);
     
 }
